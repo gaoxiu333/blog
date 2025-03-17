354d1325807c8c16aa7880d715cb97439ebd6681
xiu <gaoxiu333@gmail.com>
2025-03-18 07:52:13 +0800

    remove

--- components/error.tsx
+++
@@ -1,8 +0,0 @@
-export default function Error({ error, reset }) {
-  return (
-    <>
-      发生了错误：{error}
-      <button onClick={() => reset()}>重试</button>
-    </>
-  );
-}
--- components/page-header/componsnts/logo.tsx
+++
@@ -1,7 +0,0 @@
-export function Logo() {
-  return (
-    <a className="text-2xl text-primary" href="/">
-      olOwOlo
-    </a>
-  );
-}
